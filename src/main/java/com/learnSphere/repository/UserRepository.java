3c0892e5964970d9e566d31fa9c94a85b24c0f7e
KurubaVarunkumar <varunkumarkuruba@gmail.com>
2024-01-23 19:55:53 +0530

    demo

---
+++ src/main/java/com/learnSphere/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.learnSphere.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learnSphere.entity.Users;
+
+public interface UserRepository extends JpaRepository<Users, Integer>{
+	boolean existsByEmail(String email);
+	
+	Users getByEmail(String email);
+	
+	Users getByEmailAndPassword(String email,String Password);
+}
